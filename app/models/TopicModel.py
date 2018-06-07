37c400d9ad07947463a292387d35ac771082dd12
BingYuan <bingyuanng@gmail.com>
2018-06-07 09:30:13 +0800

    [Fix] Topic Model structure

--- app/models/TopicModel.py
+++ app/models/TopicModel.py
@@ -1,7 +1,43 @@
+import datetime
+
 class TopicModel(object):
-	# instance
+	# static vars
+	_id = 1
+	#hashtable
+	topics = {}
+	# list of key, vote tuple
+	vote_lookup = []
+	# instance method
 	def __init__(self,topic):
-		self.id = 0
 		self.topic = topic
 		self.votes = 0
+		self.id = type(self)._id
+		self.created_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		
+		type(self)._id += 1
+		type(self).topics[self.id] = self
+
+	def process(self):
+		return {
+			'id': self.id, 
+			'topic': self.topic,
+			'votes': self.votes,
+			'created_at' : self.created_at
+		}
+
+	def upvote(self, count=1):
+		self.votes += count
+	
+	def downvote(self, count=1):
+		self.votes -= count
+
+	@classmethod
+	def get_topics(cls):
+		return cls.topics
+
+	@classmethod
+	def set_topics(cls,data):
+		cls.topics = data
+
+	
 
