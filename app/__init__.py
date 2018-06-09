51f54239d1b2d6f3ce356f54c47c26182bc2d264
BingYuan <bingyuanng@gmail.com>
2018-06-09 20:13:51 +0800

    [Fix] Missing init files

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 import json
-from flask import Response, abort, jsonify
+from flask import render_template
+from flask import Response
 from flask import Flask
 from app.controllers.TopicController import topic
+from app.controllers.ErrorController import errors
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
+
+app.register_blueprint(errors)
 app.register_blueprint(topic, url_prefix='/topic')
