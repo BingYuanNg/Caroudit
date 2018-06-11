03abd1bd6f6c02deec4771e62fcb4e9a3280d76f
BingYuan <bingyuanng@gmail.com>
2018-06-12 01:43:14 +0800

    [Add] apidoc

--- app/controllers/TopicController.py
+++ app/controllers/TopicController.py
@@ -5,6 +5,15 @@ from app.resources.TopicResource import TopicResource
 
 topic = Blueprint('topic', __name__)
 
+"""
+@api {get} /topic/ Get Topic List
+@apiName GetTopicList
+@apiGroup Topic
+
+@apiParam [Number] offset offset.
+@apiParam [Number] limit limit.
+@apiParam [string] sort sort-option, Ex: -votes,+created_at where - desc, + ascending
+"""
 @topic.route('/',methods=['GET'] )
 def getTopicList():
 	data = request.args
@@ -17,6 +26,13 @@ def getTopicList():
 	}
 	return Response(json.dumps(output), mimetype='application/json')
 
+
+"""
+@api {get} /top/ Get Top List
+@apiName GetTopList
+@apiGroup Topic
+
+"""
 @topic.route('/top',methods=['GET'])
 def getTop():
 	data = request.args
@@ -28,6 +44,14 @@ def getTop():
 	}
 	return Response(json.dumps(output), mimetype='application/json')
 
+
+
+"""
+@api {get} /topic/:id Get Topic
+@apiName GetTopic
+@apiGroup Topic
+
+"""
 @topic.route('/<int:topic_id>',methods=['GET'])
 def getTopic(topic_id):
 	result = TopicResource.getTopic(topic_id)
@@ -37,6 +61,15 @@ def getTopic(topic_id):
 	}
 	return Response(json.dumps(output), mimetype='application/json')
 
+
+"""
+@api {post} /topic/:id Create Topic
+@apiName CreateTopic
+@apiGroup Topic
+
+@apiParam {string} topic topic
+
+"""
 @topic.route('/',methods=['POST'])
 def createTopic():
 	data = request.form
@@ -49,6 +82,12 @@ def createTopic():
 	return Response(json.dumps(output), mimetype='application/json')
 
 
+"""
+@api {post} /topic/:id/upvote Up Topic
+@apiName UpvoteTopic
+@apiGroup Topic
+
+"""
 @topic.route('/<int:topic_id>/upvote',methods=['POST'])
 def upvoteTopic(topic_id):
 	result = TopicResource.upvoteTopic(topic_id)
@@ -59,6 +98,13 @@ def upvoteTopic(topic_id):
 	}
 	return Response(json.dumps(output), mimetype='application/json')
 
+
+"""
+@api {post} /topic/:id/downvote Down Topic
+@apiName DownvoteTopic
+@apiGroup Topic
+
+"""
 @topic.route('/<int:topic_id>/downvote',methods=['POST'])
 def downTopic(topic_id):
 	result = TopicResource.downvoteTopic(topic_id)
@@ -68,7 +114,14 @@ def downTopic(topic_id):
 		'data' : result
 	}
 	return Response(json.dumps(output), mimetype='application/json')
-	
+
+
+"""
+@api {delete} /topic/:id/delete Delete Topic
+@apiName DeleteTopic
+@apiGroup Topic
+
+"""
 @topic.route('/<int:topic_id>',methods=['DELETE'])
 def deleteTopic(topic_id):
 	result = TopicResource.deleteTopic(topic_id)
